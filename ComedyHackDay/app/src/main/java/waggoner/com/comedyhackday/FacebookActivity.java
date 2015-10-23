b044de8aea99e8d4ddc6c8d2f27ed343dff6a59c
= <waggonernathaniel@gmail.com>
2015-10-23 15:30:54 -0400

    fb login is now done

---
+++ ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/FacebookActivity.java
@@ -0,0 +1,8 @@
+package waggoner.com.comedyhackday;
+
+/**
+ * Created by nathanielwaggoner on 10/23/15.
+ */
+public class FacebookActivity {
+
+}
--- ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/LauncherActivity.java
+++ ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/LauncherActivity.java
@@ -1,36 +1,66 @@
 package waggoner.com.comedyhackday;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class LauncherActivity extends AppCompatActivity {
 
+
+    @InjectView(R.id.login_button)
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_launcher);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.inject(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        Toolbar apptoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(apptoolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+//            }
+//        });
+        loginButton.setReadPermissions("user_friends");
+
     }
 
+//        butter knife sample code
+//    @OnClick(R.id.login_button);
+//    void onLonginClick() {}
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
