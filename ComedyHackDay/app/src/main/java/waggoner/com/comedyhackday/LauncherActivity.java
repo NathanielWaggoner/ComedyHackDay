0958d4547ae1e12e857a4a2619052fcd002506af
= <waggonernathaniel@gmail.com>
2015-10-23 14:41:52 -0400

    add the facebook sdk, don't do anything with it

--- ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/LauncherActivity.java
+++ ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/LauncherActivity.java
@@ -5,15 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.facebook.FacebookSdk;
 
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_launcher);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,6 +30,7 @@ public class LauncherActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
