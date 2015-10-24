9c0d85fbe412a57ba6c485d86dfbf097bbc500a2
= <waggonernathaniel@gmail.com>
2015-10-24 10:15:47 -0400

    adds a question thingamijigger

--- ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/FacebookActivity.java
+++
@@ -1,8 +0,0 @@
-package waggoner.com.comedyhackday;
-
-/**
- * Created by nathanielwaggoner on 10/23/15.
- */
-public class FacebookActivity {
-
-}
---
+++ ComedyHackDay/app/src/main/java/waggoner/com/comedyhackday/QuestionsList.java
@@ -0,0 +1,37 @@
+package waggoner.com.comedyhackday;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by nathanielwaggoner on 10/24/15.
+ */
+public class QuestionsList extends Fragment {
+    String[] dataSet = new String[] {"Who is that man?","Why did marion barry have to die?"};
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.questions_layout, container, false);
+        ButterKnife.bind(this, view);
+        mRecyclerView = ButterKnife.findById(view,R.id.my_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // specify an adapter (see also next example)
+        mAdapter = new QuestionsAdapter(dataSet);
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
+    }
+}
